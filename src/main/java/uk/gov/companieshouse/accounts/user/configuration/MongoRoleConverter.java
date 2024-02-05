2a9c1fbe37386de08006253ed16f57c34b74a438
krishna-patel-ch <kpatel@CompaniesHouse.gov.uk>
2024-02-05 12:32:21 +0000

    Added MongoDB custom converter for role.

---
+++ src/main/java/uk/gov/companieshouse/accounts/user/configuration/MongoRoleConverter.java
@@ -0,0 +1,16 @@
+package uk.gov.companieshouse.accounts.user.configuration;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.ReadingConverter;
+import uk.gov.companieshouse.api.accounts.user.model.Role;
+
+@ReadingConverter
+public class MongoRoleConverter implements Converter<String, Role> {
+
+    @Override
+    public Role convert( String role ) {
+        return Role.fromValue( role );
+    }
+
+}
+
