daceb045e646fb536a74dc6ab1c2d9cf5e4311cc
krishna-patel-ch <kpatel@CompaniesHouse.gov.uk>
2024-02-08 16:20:46 +0000

    Added service layer

--- src/main/java/uk/gov/companieshouse/accounts/user/service/UsersService.java
+++ src/main/java/uk/gov/companieshouse/accounts/user/service/UsersService.java
@@ -1,12 +1,15 @@
 package uk.gov.companieshouse.accounts.user.service;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 import uk.gov.companieshouse.accounts.user.mapper.UsersDtoDaoMapper;
 import uk.gov.companieshouse.accounts.user.repositories.UsersRepository;
+import uk.gov.companieshouse.api.accounts.user.model.Role;
 import uk.gov.companieshouse.api.accounts.user.model.User;
 
 @Service
@@ -31,6 +34,11 @@ public class UsersService {
     public Optional<User> fetchUser( final String userId ){
         return usersRepository.findUsersById( userId )
                               .map( usersDtoDaoMapper::daoToDto );
+    }
+
+    public void setRoles( String userId, List<Role> roles ){
+        final var update = new Update().set( "roles", new HashSet<>( roles ) );
+        usersRepository.updateUser( userId, update );
     }
 
 }
