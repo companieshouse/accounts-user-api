b32f0c74fcbda9e5d597807574264d0c2a9954dd
krishna-patel-ch <kpatel@CompaniesHouse.gov.uk>
2024-02-08 16:59:31 +0000

    Added @Transactional to the service methods

--- src/main/java/uk/gov/companieshouse/accounts/user/service/UsersService.java
+++ src/main/java/uk/gov/companieshouse/accounts/user/service/UsersService.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import uk.gov.companieshouse.accounts.user.mapper.UsersDtoDaoMapper;
 import uk.gov.companieshouse.accounts.user.repositories.UsersRepository;
 import uk.gov.companieshouse.api.accounts.user.model.Role;
@@ -23,6 +24,7 @@ public class UsersService {
         this.usersDtoDaoMapper = usersDtoDaoMapper;
     }
 
+    @Transactional
     public List<User> fetchUsers( final List<String> emails ) {
         return Optional.ofNullable(usersRepository.fetchUsers(emails))
                 .orElse(new ArrayList<>())
@@ -31,11 +33,13 @@ public class UsersService {
                               .toList();
     }
 
+    @Transactional
     public Optional<User> fetchUser( final String userId ){
         return usersRepository.findUsersById( userId )
                               .map( usersDtoDaoMapper::daoToDto );
     }
 
+    @Transactional
     public void setRoles( String userId, List<Role> roles ){
         final var update = new Update().set( "roles", new HashSet<>( roles ) );
         usersRepository.updateUser( userId, update );
