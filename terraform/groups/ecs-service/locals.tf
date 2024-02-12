de8cc0ecfbef150d3a81b0d30822e8b9cb658a38
sadiomowo-ch <sadiomowo@companieshouse.gov.uk>
2024-02-12 16:56:44 +0000

    modifying health path

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -7,8 +7,8 @@ locals {
   container_port             = "8080" # default Java port to match start script
   docker_repo                = "accounts-user-api"
   lb_listener_rule_priority  = 19
-  lb_listener_paths          = ["users/*"]
-  healthcheck_path           = "/healthcheck" #healthcheck path for accounts association service
+  lb_listener_paths          = ["/users/*"]
+  healthcheck_path           = "/users/healthcheck" #healthcheck path for accounts association service
   healthcheck_matcher        = "200"
   application_subnet_ids     = data.aws_subnets.application.ids
   kms_alias                  = "alias/${var.aws_profile}/environment-services-kms"
