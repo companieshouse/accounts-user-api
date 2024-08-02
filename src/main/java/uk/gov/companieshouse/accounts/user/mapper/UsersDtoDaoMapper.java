25fccdaa8ad7da00740422358927adfafb03b313
John Griffiths <jgriffiths2@companieshouse.gov.uk>
2024-08-02 16:32:14 +0100

    IDVA6-1317 Increase code coverage

--- src/main/java/uk/gov/companieshouse/accounts/user/mapper/UsersDtoDaoMapper.java
+++ src/main/java/uk/gov/companieshouse/accounts/user/mapper/UsersDtoDaoMapper.java
@@ -1,10 +1,7 @@
 package uk.gov.companieshouse.accounts.user.mapper;
 
-import org.json.JSONObject;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
-import uk.gov.companieshouse.accounts.user.models.OneLoginDataDao;
 import uk.gov.companieshouse.accounts.user.models.Users;
 import uk.gov.companieshouse.api.accounts.user.model.User;
 
@@ -16,24 +13,9 @@ public interface UsersDtoDaoMapper {
     @Mapping(source = "created", target =   "created", qualifiedByName = "localDateTimeToOffsetDateTime")
     @Mapping(target = "hasLinkedOneLogin",
             expression = "java(java.util.Objects.nonNull(users.getOneLoginData()))")
-//    @Mapping(source = "oneLoginData", target = "hasLinkedOneLogin", qualifiedByName = "isValidOneLoginData")
     User daoToDto(Users users);
 
     @Mapping(source = "created", target =   "created", qualifiedByName = "offsetDateTimeToDateTime")
     @Mapping(source = "userId", target = "id")
     Users dtoToDao(User user);
-
-    @Named("isValidOneLoginData")
-    default boolean isValidOneLoginData(OneLoginDataDao oneLoginData) {
-        var jsonObject = new JSONObject(oneLoginData);
-
-        if (jsonObject.has("oneLoginUserId")) {
-            var userIdValue = jsonObject.getString("oneLoginUserId");
-            return userIdValue != null && !userIdValue.isEmpty();
-        } else {
-            return false;
-        }
-    }
-
-
 }
