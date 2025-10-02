308c53853d857b5536bc60f256917ae4776bdf82
NeilShadrach <nshadrach@companieshouse.gov.uk>
2025-10-02 08:25:37 +0100

    CC-2973 javax to jakarta

--- src/main/java/uk/gov/companieshouse/accounts/user/util/StaticPropertyUtil.java
+++ src/main/java/uk/gov/companieshouse/accounts/user/util/StaticPropertyUtil.java
@@ -1,6 +1,6 @@
 package uk.gov.companieshouse.accounts.user.util;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
